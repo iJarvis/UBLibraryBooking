bd01b3cb4bd19b0ce56fa293dcafda445ac594b7
Syed <aqhibahmed@gmail.com>
2019-04-06 01:25:01 -0400

    fixed Procfile

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import libraryScript
 sched = BlockingScheduler(timezone="America/New_York")
 
-@sched.scheduled_job('cron', hour=1,minute=24)
+@sched.scheduled_job('cron', hour=00)
 def scheduled_job():
     libraryScript.bookRooms()
 
