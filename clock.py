6b5fa7af6a5ce800f4776a7a2d7c4c84f0d506e0
Chaitanya Pawa <pawachaitanya@gmail.com>
2019-05-05 00:50:50 -0400

    new changes

--- clock.py
+++ clock.py
@@ -5,12 +5,12 @@ import script
 sched = BlockingScheduler(timezone="America/New_York")
 sched2 = BlockingScheduler(timezone="America/New_York")
 
-@sched.scheduled_job('cron', hour=00,minute=49)
+@sched.scheduled_job('cron', hour=00)
 def scheduled_job():
     libraryScript.bookRooms()
     script.bookRoom()
 
-@sched.scheduled_job('cron', hour=00,minute=50)
+@sched.scheduled_job('cron', hour=00,minute=10)
 def scheduled_job():
     libScript2.bookRooms()
     # script.bookRoom()
